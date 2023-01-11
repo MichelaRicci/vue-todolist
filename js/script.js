22c063ab903abf82febafa319214dbe7d775d1e9
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-11 15:23:40 +0100

    array added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 console.log('Vue Ok');
 
 
+const app = Vue.createApp({
+
+    data(){
+      return {
+  
+  
+        newItem: '',
+        items: [
+  
+        {
+          text: 'Ritirare pacco in posta',
+          done: true,
+        },  
+        {
+            text: 'Fare la spesa',
+            done: false,
+          },
+          {
+            text: 'Passare in lavanderia',
+            done: false,
+          },
+          {
+            text: 'Ritirare la posta',
+            done: false,
+          },
+          {
+            text: 'Chiamare amministrazione',
+            done: false,
+          },
+  
+       ]}
+    },
+  
+  }).mount('#root');
